d7d8eff4b20a3e6776eab89a9fc3d42503191215
Vaneva Svetlana <sveta.vaneva042007@gmail.com>
2026-04-29 22:24:37 +0300

    vanyova.svetlana:T3

---
+++ vanyova.svetlana/T3/commands.h
@@ -0,0 +1,15 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+#include "Polygon.h"
+
+bool isOrthogonal(const Point& a, const Point& b, const Point& c);
+bool rightAngle(const Polygon& poly);
+void rightshapes(const std::vector<Polygon>& polygon, std::ostream& out);
+
+void echo(std::vector<Polygon>& polygon, std::ostream& out);
+void min(const std::vector<Polygon>& polygon, std::string name, std::ostream& out);
+void max(const std::vector<Polygon>& polygon, std::string name, std::ostream& out);
+void area(const std::vector<Polygon>& polygon, std::string name, std::ostream& out);
+void count(const std::vector<Polygon>& polygon, std::string name, std::ostream& out);
+
+#endif // !
